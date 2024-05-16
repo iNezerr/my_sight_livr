d24cfb1553c54ac5d6374f604826eb0eb7c75754
Eben <bonuzx1@gmail.com>
2024-05-16 23:20:38 +0000

    "Updated .eslintrc.json and MobileNav.tsx: added 'react/jsx-key' rule, imported 'randomInt' from 'crypto', and added key prop to SheetClose component"

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -7,11 +7,13 @@ import {
 } from '@/components/ui/sheet'
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
+import { randomInt } from 'crypto'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const MobileNav = () => {
+  const keyid = randomInt(10)
   const pathname = usePathname()
   return (
     <section className="w-full max-w-[264px]">
@@ -42,7 +44,7 @@ const MobileNav = () => {
                 {sidebarLinks.map((link) => {
                   const isActive = pathname === link.route
                   return (
-                    <SheetClose asChild>
+                    <SheetClose key={keyid} asChild>
                       <Link
                         href={link.route}
                         key={link.label}
